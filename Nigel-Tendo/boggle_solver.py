d2d2b398a8f46925274905bf319730433ab59a84
Nigel Tendo <nigel.tendo@bison.howard.edu>
2026-02-19 22:39:23 +0000

    Add initial boggle solver solution

---
+++ Nigel-Tendo/boggle_solver.py
@@ -0,0 +1,19 @@
+# Burge's Solution - Boggle Solver using HashMap
+
+import re
+
+class Boggle:
+    def __init__(self, grid, dictionary):
+        self.grid = grid
+        self.dictionary = dictionary
+        self.solutions = []
+
+def main():
+    grid = [["T", "W", "Y", "R"], ["E", "N", "P", "H"],["G", "Z", "Qu", "R"],["O", "N", "T", "A"]]
+    dictionary = ["art", "ego", "gent", "get", "net", "new", "newt", "prat", "pry", "qua", "quart", "quartz", "rat", "tar", "tarp", "ten", "went", "wet", "arty", "rhr", "not", "quar"]
+    
+    mygame = Boggle(grid, dictionary)
+    print(mygame.getSolution())
+
+if __name__ == "__main__":
+    main()
