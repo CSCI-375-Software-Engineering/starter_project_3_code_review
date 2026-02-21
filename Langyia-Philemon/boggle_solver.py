3257f40e26a19fe5f4273a0469e855c4b5ae4589
Langyia Philemon <langyia.philemon@bison.howard.edu>
2026-02-21 03:12:36 +0000

    Apply code review feedback and refactor solver

--- Langyia-Philemon/boggle_solver.py
+++ Langyia-Philemon/boggle_solver.py
@@ -5,68 +5,74 @@ SID: 004002416
 
 class Boggle:
   # constructor: set up the grid, dictionary and helper sets
-    def __init__(self, grid, dictionary):
-        self.grid = grid #this is teh NxN boggle board
-        self.dictionary = dictionary #list of valid words
-        self.solution = [] #hold all the words we find
-        if dictionary: #convert dictionary to uppercase to make it case insensitive
-          self.word_set = set(word.upper()for word in dictionary)
-        else:
-          self.word_set = set() #incase there is an empty dictionary
-        self.prefix_set = set() #makes a prefix set so that the dfs ends early
-        for word in self.word_set:
-          for i in range(1, len(word)+1):
-            self.prefix_set.add(word[:i]) #adds every prefix of each word
-    def setDictionary(self, dictionary): #setter for dictionary
-      self.dictionary = dictionary
-      if dictionary:
-          self.word_set = set(word.upper()for word in dictionary)
-      else:
-        self.word_set = set()
-      self.prefix_set = set()
-      for word in self.word_set:
-        for i in range(1, len(word)+1):
-          self.prefix_set.add(word[:i])
-    def getSolution(self): #function to find all words
-        self.solution = []
-        if not self.grid or not self.dictionary: #check for invalid input
-            return []
-        rows = len(self.grid) #number of rows
-        columns = len(self.grid[0]) #number of columns
-        visited = [[False for j in range(columns)] for i in range(rows)] #create visited array to check the tiles used in the current word
-        found = set() # store words that we find and we used a set to prevent duplicates
-        for r in range(rows): #start dfs from every tile
-            for c in range(columns):
-                self._dfs(r, c, "", visited, found)
-        self.solution = sorted(found) #sort to arrange the words found
-        return self.solution
-    def _dfs(self, r, c, current_word, visited, found): #helper function
-        rows = len(self.grid)
-        columns = len(self.grid[0])
-        if r < 0 or r >= rows or c < 0 or c >= columns: #base case if out of bounds
-            return
-        if visited[r][c]: #base case if we already used thsi tile in the current path
-            return
-        letter = self.grid[r][c].upper() #the tile
-        new_word = current_word + letter #add the tile to the current word
-        if new_word not in self.prefix_set: #stop if the new_word wonyt match the prefixes
-          return
-        if len(new_word) >= 3 and new_word in self.word_set: #if the word is valid, add it to the set
-            found.add(new_word) 
-        visited[r][c] = True #tile is used
-        for drow in [-1, 0, 1]: # explore all the adjacent tiles including the diagonals
-            for dcolumn in [-1, 0, 1]:
-                if drow != 0 or dcolumn != 0: #skip the current tile
-                    self._dfs(r+ drow, c+ dcolumn, new_word, visited, found)
-        visited[r][c] = False #unmark tile when going back
-    
-def main(): #main function
-    grid = [["T", "W", "Y", "R"], ["E", "N", "P", "H"],["G", "Z", "Qu", "R"],["O", "N", "T", "A"]]
-    dictionary = ["art", "ego", "gent", "get", "net", "new", "newt", "prat", "pry", "qua", "quart", "quartz", "rat", "tar", "tarp", "ten", "went", "wet", "arty", "rhr", "not", "quar"]
-    
-    mygame = Boggle(grid, dictionary) #boggle object
-    print(mygame.getSolution()) #print the words that we found
+  def __init__(self, grid, dictionary):
+    self.grid = grid  # NxN Boggle board
+    self.dictionary = dictionary  # list of valid words
+    self.solution = []  # store found words
+    self.word_set, self.prefix_set = self._build_sets(dictionary)
+
+  # helper method to build word set and prefix set
+  def _build_sets(self, dictionary):
+    if dictionary:
+      word_set = set(word.upper() for word in dictionary)
+    else:
+      word_set = set()
+    prefix_set = set()
+    for word in word_set:
+      for i in range(1, len(word) + 1):
+        prefix_set.add(word[:i])
+    return word_set, prefix_set
+  def setDictionary(self, dictionary):
+    self.dictionary = dictionary
+    self.word_set, self.prefix_set = self._build_sets(dictionary)
+    # main function to find all words
+  def getSolution(self):
+    self.solution = []
+    # guard for empty or invalid grids
+    if not self.grid or not self.grid[0]:
+      return []
+    rows = len(self.grid)
+    cols = len(self.grid[0])
+    visited = [[False for _ in range(cols)] for _ in range(rows)]
+    found = set()
+    for r in range(rows):
+      for c in range(cols):
+        self._dfs(r, c, "", visited, found, rows, cols)
+    self.solution = sorted(found)
+    return self.solution
+
+  # DFS helper function
+  def _dfs(self, r, c, current_word, visited, found, rows, cols):
+    if r < 0 or r >= rows or c < 0 or c >= cols:
+      return
+    if visited[r][c]:
+      return
+    letter = self.grid[r][c].upper()
+    new_word = current_word + letter
+    if new_word not in self.prefix_set:
+      return
+    if len(new_word) >= 3 and new_word in self.word_set:
+      found.add(new_word)
+    visited[r][c] = True
+    DIRECTIONS = [(-1,-1), (-1,0), (-1,1),
+                  (0,-1),         (0,1),
+                  (1,-1),  (1,0), (1,1)]
+    for dr, dc in DIRECTIONS:
+      self._dfs(r + dr, c + dc, new_word, visited, found, rows, cols)
+    visited[r][c] = False
+
+def main():
+  grid = [["T", "W", "Y", "R"],
+          ["E", "N", "P", "H"],
+          ["G", "Z", "Qu", "R"],
+          ["O", "N", "T", "A"]]
+  dictionary = ["art", "ego", "gent", "get", "net", "new", "newt",
+                "prat", "pry", "qua", "quart", "quartz", "rat",
+                "tar", "tarp", "ten", "went", "wet", "arty", "rhr",
+                "not", "quar"]
+  mygame = Boggle(grid, dictionary)
+  print(mygame.getSolution())
+
 
 if __name__ == "__main__":
-    main()
-    
+  main()
