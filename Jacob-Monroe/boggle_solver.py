5c28a822d52ec61d181a9d8dbb72d95b802aab56
Jacob Monroe <jacob.monroe@bison.howard.edu>
2026-03-02 19:34:41 +0000

    Update solver implementation

--- Jacob-Monroe/boggle_solver.py
+++ Jacob-Monroe/boggle_solver.py
@@ -2,11 +2,11 @@
 Jacob Monroe
 SSID: 003108194
 """
+
 class BoggleSolver:
     def __init__(self, dictionary):
         self.words = set(dictionary)
         self.trie = {}
-        # Build Trie for faster prefix checking
         for word in self.words:
             if len(word) < 3:
                 continue
@@ -15,7 +15,7 @@ class BoggleSolver:
                 if char not in node:
                     node[char] = {}
                 node = node[char]
-            node["#"] = True  # End of word marker
+            node["#"] = True
 
     def getSolution(self, grid):
         if not grid:
@@ -32,11 +32,9 @@ class BoggleSolver:
         return sorted(list(found_words))
 
     def _dfs(self, grid, r, c, rows, cols, node, path, visited, found_words):
-        # Handle "Qu" special case (common in Boggle)
         char = grid[r][c].lower()
-        
-        # Check if the character(s) on the tile exist in our current Trie branch
         current_node = node
+        
         for s in char:
             if s not in current_node:
                 return
@@ -45,11 +43,9 @@ class BoggleSolver:
         new_path = path + char
         visited.add((r, c))
 
-        # If we reached the end of a word in the Trie
         if "#" in current_node:
             found_words.add(new_path)
 
-        # Explore all 8 neighbors (including diagonals)
         for dr in [-1, 0, 1]:
             for dc in [-1, 0, 1]:
                 if dr == 0 and dc == 0:
@@ -58,15 +54,4 @@ class BoggleSolver:
                 if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in visited:
                     self._dfs(grid, nr, nc, rows, cols, current_node, new_path, visited, found_words)
 
-        # Backtrack: remove from visited so other paths can use this tile
         visited.remove((r, c))
-
-def main():
-    grid = [["S", "Q", "R"], ["A", "N", "E"], ["T", "U", "P"]]
-    dictionary = ["queen", "ant", "ran", "sun", "square"]
-    
-    my_solver = BoggleSolver(dictionary)
-    print("Found words:", my_solver.getSolution(grid))
-
-if __name__ == "__main__":
-    main()
