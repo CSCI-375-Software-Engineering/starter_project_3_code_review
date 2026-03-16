e85a2d051b24b1b44eb84448c8d4798c9cab341d
binta kanu <fatmata.kanu@bison.howard.edu>
2026-03-16 23:26:25 +0000

    Fix lint / style issues

--- Fatmata-Kanu/boggle_solver.py
+++ Fatmata-Kanu/boggle_solver.py
@@ -1,10 +1,10 @@
 """Fatmata Kanu"""
 """004001377"""
-import re
 
 
 class Boggle:
-    """Boggle solver which takes in a grid and dictionary and generates possible solutions."""
+    """Boggle solver which takes in a grid and dictionary
+    and generates possible solutions."""
 
     def __init__(self, grid, dictionary):
         self.grid = grid
@@ -18,23 +18,26 @@ class Boggle:
     def setGrid(self, grid):
         self.grid = grid
 
-    def getSolution(self): # defining function to find solutions
-        if not isinstance(self.dictionary, list) or not self.dictionary: #making sure dict is non-empty list 
+    def getSolution(self):  # defining function to find solutions
+        if not isinstance(self.dictionary, list) or not self.dictionary:
+            # making sure dict is non-empty list
             return []
-        for w in self.dictionary: 
-            if not isinstance(w, str) or not w: #checker for if a word is a string 
+        for w in self.dictionary:
+            if not isinstance(w, str) or not w:
+                # checker for if a word is a string
                 return []
 
-        if not isinstance(self.grid, list) or not self.grid: #grid exists
-            return [] 
+        if not isinstance(self.grid, list) or not self.grid:
+            # grid exists
+            return []
 
-        num = len(self.grid) #size of the board
+        num = len(self.grid)  # size of the board
 
-        board = [] # making letters on the board lowercase
+        board = []  # making letters on the board lowercase
         for i in range(num):
             row = []
             for j in range(num):
-                row.append(self.grid[i][j].lower()) #adding lowercase tile
+                row.append(self.grid[i][j].lower())  # adding lowercase tile
             board.append(row)
 
         wordSet = set()
@@ -46,23 +49,24 @@ class Boggle:
         if not wordSet:
             return []
 
-        preSet = set() # prefix set to eliminate bad searches head on 
+        preSet = set()  # prefix set to eliminate bad searches head on
         for w in wordSet:
             for x in range(1, len(w) + 1):
                 preSet.add(w[:x])
 
         found = set()
 
-        already_visited = [] # tracking already visited tiles
+        already_visited = []  # tracking already visited tiles
         for _ in range(num):
             already_visited.append([False] * num)
 
         drs = [(-1, -1), (0, -1), (1, -1),
                (-1, 0),           (1, 0),
-               (-1, 1),  (0, 1),  (1, 1)] # all possible 8 directions
+               (-1, 1),  (0, 1),  (1, 1)]  # all possible 8 directions
 
         def search(i, j, word):
-            # moving through each tile that is not already used, word is built and then reset to be used again
+            # moving through each tile that is not already used,
+            # word is built and then reset to be used again
             if word not in preSet:
                 return
 
@@ -73,12 +77,15 @@ class Boggle:
                 new_row_index = i + row_change
                 new_column = j + column_change
 
-                if 0 <= new_row_index < num and 0 <= new_column < num and not already_visited[new_row_index][new_column]:
+                if (0 <= new_row_index < num
+                        and 0 <= new_column < num
+                        and not already_visited[new_row_index][new_column]):
                     already_visited[new_row_index][new_column] = True
-                    search(new_row_index, new_column, word + board[new_row_index][new_column])
+                    search(new_row_index, new_column,
+                           word + board[new_row_index][new_column])
                     already_visited[new_row_index][new_column] = False
 
-        #search for every tile on the board 
+        # search for every tile on the board
         for i in range(num):
             for j in range(num):
                 already_visited[i][j] = True
@@ -90,11 +97,15 @@ class Boggle:
 
 
 def main():
-    grid = [["T", "W", "Y", "R"], ["E", "N", "P", "H"],["G", "Z", "Qu", "R"],["O", "N", "T", "A"]]
-    dictionary = ["art", "ego", "gent", "get", "net", "new", "newt", "prat", "pry", "qua", "quart", "quartz", "rat", "tar", "tarp", "ten", "went", "wet", "arty", "rhr", "not", "quar"]
-    
+    grid = [["T", "W", "Y", "R"], ["E", "N", "P", "H"],
+            ["G", "Z", "Qu", "R"], ["O", "N", "T", "A"]]
+    dictionary = ["art", "ego", "gent", "get", "net", "new", "newt",
+                  "prat", "pry", "qua", "quart", "quartz", "rat", "tar",
+                  "tarp", "ten", "went", "wet", "arty", "rhr", "not", "quar"]
+
     mygame = Boggle(grid, dictionary)
     print(mygame.getSolution())
+
 
 if __name__ == "__main__":
     main()
